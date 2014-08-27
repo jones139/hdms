f594ffa9a8873c208b335e0f859fafd9f5879072
Graham Jones <grahamjones139@gmail.com>
2014-08-27 21:13:39 +0100

    Indented notifications controller code nicely.

--- app/Controller/NotificationsController.php
+++ app/Controller/NotificationsController.php
@@ -28,70 +28,74 @@ App::uses('AppController', 'Controller');
  * @property PaginatorComponent $Paginator
  */
 class NotificationsController extends AppController {
-
+    
 /**
  * Components
  *
  * @var array
  */
-	public $components = array('Paginator');
-
+    public $components = array('Paginator');
+    
 /**
  * index method - by default lists all notifications. 
- *                if a user_id GET parameter is provided, it lists only the active notifications
- *                  for the given user_id.
+ *                if a user_id named parameter is provided, it lists only 
+ *                  the active notifications for the given user_id.
  *
  * @return void
  */
-	public function index() {
-		$this->Notification->recursive = 2;
-		#echo "<pre>".var_dump($this->params)."</pre>";
-		if (isset($this->params[ 'named' ][ 'user_id' ])) {
-		   $user_id = $this->params[ 'named' ][ 'user_id' ];
-		   $this->Paginator->settings = array(
-		   			      'conditions'=>array('Notification.user_id'=>$user_id,
-								  'Notification.active'=>1)
-							);
-		} 
- 		   $this->set('notifications', $this->Paginator->paginate());
-  	
-	}
+    public function index() {
+        $this->Notification->recursive = 2;
+        #echo "<pre>".var_dump($this->params)."</pre>";
+        if (isset($this->params[ 'named' ][ 'user_id' ])) 
+            { 
+                $user_id = $this->params[ 'named' ][ 'user_id' ];
+                $this->Paginator->settings = array(
+                    'conditions'=>array('Notification.user_id'=>$user_id,
+                    'Notification.active'=>1)
+                );
+            } 
+        $this->set('notifications', $this->Paginator->paginate()); 	
+    }
 
-/**
- * view method - by default views the notification specified by $id. 
- *
- *
- * @throws NotFoundException
- * @param string $id
- * @return void
- */
-	public function view($id = null) {
-		if (!$this->Notification->exists($id)) {
-			throw new NotFoundException(__('Invalid notification'));
-		}
-		$options = array('conditions' => array('Notification.' . $this->Notification->primaryKey => $id));
-		$this->set('notification', $this->Notification->find('first', $options));
-	}
+    /**
+     * view method - by default views the notification specified by $id. 
+     *
+     *
+     * @throws NotFoundException
+     * @param string $id
+     * @return void
+     */
+    public function view($id = null) {
+        if (!$this->Notification->exists($id)) {
+            throw new NotFoundException(__('Invalid notification'));
+        }
+        $options = array('conditions' => array(
+            'Notification.' . $this->Notification->primaryKey => $id));
+        $this->set('notification', $this->Notification->find(
+            'first', $options));
+    }
 
 /**
  * add method
  *
  * @return void
  */
-	public function add() {
-		if ($this->request->is('post')) {
-			$this->Notification->create();
-			if ($this->Notification->save($this->request->data)) {
-				$this->Session->setFlash(__('The notification has been saved.'));
-				return $this->redirect(array('action' => 'index'));
-			} else {
-				$this->Session->setFlash(__('The notification could not be saved. Please, try again.'));
-			}
-		}
-		$users = $this->Notification->User->find('list');
-		$revisions = $this->Notification->Revision->find('list');
-		$this->set(compact('users', 'revisions'));
-	}
+    public function add() {
+        if ($this->request->is('post')) {
+            $this->Notification->create();
+            if ($this->Notification->save($this->request->data)) {
+                $this->Session->setFlash(
+                    __('The notification has been saved.'));
+                return $this->redirect(array('action' => 'index'));
+            } else {
+                $this->Session->setFlash(
+                    __('The notification could not be saved. Please, try again.'));
+            }
+        }
+        $users = $this->Notification->User->find('list');
+        $revisions = $this->Notification->Revision->find('list');
+        $this->set(compact('users', 'revisions'));
+    }
 
 /**
  * edit method
@@ -100,25 +104,29 @@ class NotificationsController extends AppController {
  * @param string $id
  * @return void
  */
-	public function edit($id = null) {
-		if (!$this->Notification->exists($id)) {
-			throw new NotFoundException(__('Invalid notification'));
-		}
-		if ($this->request->is(array('post', 'put'))) {
-			if ($this->Notification->save($this->request->data)) {
-				$this->Session->setFlash(__('The notification has been saved.'));
-				return $this->redirect(array('action' => 'index'));
-			} else {
-				$this->Session->setFlash(__('The notification could not be saved. Please, try again.'));
-			}
-		} else {
-			$options = array('conditions' => array('Notification.' . $this->Notification->primaryKey => $id));
-			$this->request->data = $this->Notification->find('first', $options);
-		}
-		$users = $this->Notification->User->find('list');
-		$revisions = $this->Notification->Revision->find('list');
-		$this->set(compact('users', 'revisions'));
-	}
+    public function edit($id = null) {
+        if (!$this->Notification->exists($id)) {
+            throw new NotFoundException(__('Invalid notification'));
+        }
+        if ($this->request->is(array('post', 'put'))) {
+            if ($this->Notification->save($this->request->data)) {
+                $this->Session->setFlash(
+                    __('The notification has been saved.'));
+                return $this->redirect(array('action' => 'index'));
+            } else {
+                $this->Session->setFlash(
+                    __('The notification could not be saved. Please, try again.'));
+            }
+        } else {
+            $options = array('conditions' => array(
+                'Notification.' . $this->Notification->primaryKey => $id));
+            $this->request->data = $this->Notification->find(
+                'first', $options);
+        }
+        $users = $this->Notification->User->find('list');
+        $revisions = $this->Notification->Revision->find('list');
+        $this->set(compact('users', 'revisions'));
+    }
 
 /**
  * delete method
@@ -127,17 +135,17 @@ class NotificationsController extends AppController {
  * @param string $id
  * @return void
  */
-	public function delete($id = null) {
-		$this->Notification->id = $id;
-		if (!$this->Notification->exists()) {
-			throw new NotFoundException(__('Invalid notification'));
-		}
-		$this->request->allowMethod('post', 'delete');
-		if ($this->Notification->delete()) {
-			$this->Session->setFlash(__('The notification has been deleted.'));
-		} else {
-			$this->Session->setFlash(__('The notification could not be deleted. Please, try again.'));
-		}
-		return $this->redirect($this->referer());
-	}
+    public function delete($id = null) {
+        $this->Notification->id = $id;
+        if (!$this->Notification->exists()) {
+            throw new NotFoundException(__('Invalid notification'));
+        }
+        $this->request->allowMethod('post', 'delete');
+        if ($this->Notification->delete()) {
+            $this->Session->setFlash(__('The notification has been deleted.'));
+        } else {
+            $this->Session->setFlash(__('The notification could not be deleted. Please, try again.'));
+        }
+        return $this->redirect($this->referer());
+    }
 }
